8569e3d8f19a924aee5e8605a8c2a388574182da
Tyree Christensen <tyree83@gmail.com>
2021-01-07 15:50:36 -0500

    Added wish.js wishes.js pages

---
+++ src/components/Wish.js
@@ -0,0 +1,7 @@
+export default function Wish () {
+    return(
+        <nav>
+            <h1>Wish</h1>
+        </nav>
+    )
+}
---
+++ src/components/Wishes.js
@@ -0,0 +1,7 @@
+export default function Wishes () {
+    return(
+        <nav>
+            <h1>Wishes</h1>
+        </nav>
+    )
+}
