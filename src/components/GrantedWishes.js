77be424db712bf5f45f41b6cf93037cfa69d98a0
Tyree Christensen <tyree83@gmail.com>
2021-01-08 13:31:45 -0500

    changed name to es

---
+++ src/components/GrantedWishes.js
@@ -0,0 +1,27 @@
+
+export default function GrantedWishes () {
+    return(
+        <div>
+            <div className="nav">
+                <h3>I wish I had a new puppy!</h3>
+                <p>Jessica R</p>
+                <p>Donated By: Anon</p>
+            </div>
+            <div className="nav">
+                <h3>I wish I had a new puppy!</h3>
+                <p>Jessica R</p>
+                <p>Donated By: Anon</p>
+            </div>
+            <div className="nav">
+                <h3>I wish I had a new puppy!</h3>
+                <p>Jessica R</p>
+                <p>Donated By: Anon</p>
+            </div>
+            <div className="nav">
+                <h3>I wish I had a new puppy!</h3>
+                <p>Jessica R</p>
+                <p>Donated By: Anon</p>
+            </div>
+          </div>
+    )
+}
