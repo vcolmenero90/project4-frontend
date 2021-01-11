30269f9bd471e81b28d85cca04c6cdccde3063a0
Tyree Christensen <tyree83@gmail.com>
2021-01-11 18:07:25 -0500

    updated header, grantedwishes, wishs CSS

--- src/components/GrantedWishes.js
+++ src/components/GrantedWishes.js
@@ -3,29 +3,29 @@ export default function GrantedWishes () {
     return(
 
         <div className='nav'>
-            <div>
+            <div className="card">
                 <h3>I wish I had a new puppy!</h3>
                 <p>Jessica R</p>
                 <p>Donated By: Anon</p>
             </div>
 
             <hr/>
-            <div>
-                <h3>I wish I had a new puppy!</h3>
-                <p>Jessica R</p>
-                <p>Donated By: Anon</p>
+            <div className="card">
+                <h3>I wish I had a big mac</h3>
+                <p>Robert Jones</p>
+                <p>Donated By: Steve Jones</p>
             </div>
             <hr/>
-            <div>
-                <h3>I wish I had a new puppy!</h3>
-                <p>Jessica R</p>
-                <p>Donated By: Anon</p>
+            <div className="card">
+                <h3>I wish I had an engagement ring</h3>
+                <p>Rachel</p>
+                <p>Donated By: Future Husband</p>
             </div>
             <hr/>
-            <div>
-                <h3>I wish I had a new puppy!</h3>
-                <p>Jessica R</p>
-                <p>Donated By: Anon</p>
+            <div className="card">
+                <h3>I wish I had a new sister!</h3>
+                <p>Allison T</p>
+                <p>Donated By: Mom</p>
             </div>
             <hr/>
           </div>
--- src/components/Wishes.js
+++ src/components/Wishes.js
@@ -2,14 +2,16 @@ import Wish from './Wish'
 
 export default function Wishes ({ wishes, handleDelete, handleUpdate }) {
   return(
-      <div>
+      <div className='nav'>
           {wishes.map(wish => 
+        <div className="card">
         <Wish 
           key={wish.id} 
           wish={wish}
           handleDelete={handleDelete}
           handleUpdate={handleUpdate}
         />
+        </div>
       )}
       </div>
   )
